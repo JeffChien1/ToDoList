3b0f4e0ffb17059f5c646b717986cc6053cff550
Jeff Chien <eng.jeffchien@gmail.com>
2023-03-06 00:17:47 -0300

    Criando funções para adicionar elementos novos na lista

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 const form = document.getElementById('add__element');
+const element = document.getElementById('list__element');
+const today = date.getDate()
+
 
 form.addEventListener('submit',(evento) => {
     evento.preventDefault();
 
     console.log(evento);
 
-
+    creatingElement(evento.target['main__task'].value);
 })
 
-function adicionandoElementoNaLista () {
+function creatingElement (main__task) {
+    const newItem = document.createElement('li');
+    const taskNumber = document.createElement('h2');
+    const newTask = document.createElement('p');
+    const newDate = document.createElement('p');
+    
+    newItem.classList.add('task__list-item');
+    taskNumber.classList.add('task__list-number');
+    newTask.classList.add('task__list-description');
+    newDate.classList.add('task__data-number');
 
+    taskNumber.innerHTML = "Task 1";
+    newTask.innerHTML = main__task;
+    newDate.innerHTML = newDate();
+
+    newItem.appendChild(newTask);
+    newItem.appendChild(taskNumber);
+    newItem.appendChild(newDate);
+
+    console.log(newItem);
 }
